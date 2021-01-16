83f2ce6ccce9666113d4b004ea33017170fbef7d
Tom Richards <37841024+richardst89@users.noreply.github.com>
2021-01-16 20:23:44 +0000

    Updated package versions, to resolve Probot compatability issues (#140)

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
-import { Application } from 'probot'
 import { handlePullRequest } from './handler'
 
-export = (app: Application): void => {
+export = (app: any): void => {
   app.on('pull_request.opened', handlePullRequest)
   app.on('pull_request.ready_for_review', handlePullRequest)
 }
